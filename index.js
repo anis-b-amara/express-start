d209ebd9a03e74fa049188ba3db154795bc6fda4
Anis Ben Amara <anis.ben.amara89@gmail.com>
2019-09-23 19:47:52 +0200

    install express.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (_, res) => res.send('Hello world!'));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
