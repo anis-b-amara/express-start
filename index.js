f208e4854e1c0c8582523e1621c7d78f1e6ed560
Anis Ben Amara <anis.ben.amara89@gmail.com>
2019-09-27 21:40:59 +0200

    First swagger setup

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-import * as express from 'express'
+import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from './swagger.json'
 
 const app = express()
 
 const port = 3000
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get('/', (_, res) => res.send('Hello world!'))
 
